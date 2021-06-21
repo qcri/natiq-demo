f1ce8505a1737bce5f2946f50a11534f4761d4bf
Alaa Hamoudah <alaakhalil11@gmail.com>
2021-06-21 15:13:22 +0300

    Add tailwind css library and fix style of index page

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
  
-     $("document").ready(function(){
-       
-         
-
+  $("document").ready(function(){
      $("#diacritize_btn").click(function (e) {
         e.preventDefault();
         var text = $("#plainText").val();
@@ -14,7 +11,6 @@
             splittedArray = [text]    
         arrayOfText = addDotToString(splittedArray)
         text = arrayOfText.join("\n");
-
         var api_key = "gVyMcgLnLNHduIisHh";
         var settings = {
           async: true,
@@ -40,113 +36,56 @@
         });
       });
       function addDotToString(textArray){
-        //   console.log()
         let newArray = []
-        //   for(const text of textArray){
         for(let i=0;i < textArray.length; i++){
-            let lastChar = textArray[i].substr(textArray[i].length - 1);  
-
-            console.log(textArray[i]+"   "+lastChar)
+          textArray[i] = textArray[i].trim();
+          let lastChar = textArray[i].substr(textArray[i].length - 1);  
             if (lastChar == '.' || lastChar =='،'){
                 newArray.push(textArray[i])
             }
             else  {
                 console.log(textArray[i]+".")
                 newArray.push(textArray[i]+".")
-                // textArray[i].replace(textArray[i], (textArray[i]+"."))
                 console.log(newArray[i])
             }
           }
-          console.log(newArray)
           return newArray
       }
 
       function exposeResult(resultsList) {
-        let result = "";
+        let result = '';
+        let resultTitle ='<div class="flex items-center justify-center space-x-2">'+
+        '<span class="h-px w-16 bg-gray-300"></span>'+
+        '<span class="text-blue-500 font-normal">Dictirized text</span>'+
+        '<span class="h-px w-16 bg-gray-300"></span>';
         for (const text of resultsList) {
         result +=
-            '<div class="processed-result"><textarea type="text" id="plainText" class="form-control">' +
-            text +
-            "</textarea>" +
-            '<button value="speak" class="speaker-btn"></button></div>'
-        
+        '<div class="processed-result">'+
+        '<textarea id="plainText" class="w-11/12 text-base py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500" >' + text + '</textarea>' +
+        '<button value="speak" class="speaker-btn"></button>'+
+        '<div class="center hidden loading-wrapper">  <span class="loading"></span></div>'+
+        '</div>';        
         }
-        // $("#diacritized-results").html("");
+        console.log(result)
+        $("#result-title").html(resultTitle);
         $("#diacritized-results").html(result);
     }  
     
      $("#diacritized-results").on("click", ".speaker-btn", function(e){
          let current = $(this);
-        // $(".speaker-btn").on("click", function(e){
         e.preventDefault();
         let text = $(this).prev().val();
-        jobId= submitTTSJob(text, current);
-    //    promise1 = new Promise((resolve, reject) => {
-    //         result = temp(1)
-            // console.log("res", result)
-            // return resolve(result)
-            // resolve(checkStatus("a195bc0c-8d63-4cbd-8518-2c61a4de6dcf") );  
-            // result = checkStatus("a195bc0c-8d63-4cbd-8518-2c61a4de6dcf") 
-            // if(result == "Done"){
-            //     console.log("yesssss")
-            //    return resolve(result.job_id)
-            // }
-            // else
-            //    return reject("No response back")   
-        // })
-
-        // promise1 = new Promise(async(resolve, reject) => {
-        //     res = await temp(1)
-        //     console.log(res)
-        //     if (res == 5){
-        //         console.log("here")
-        //          resolve( res)
-        //     }
-        // })
-        // .then(() => {
-        //     console.log("heeer")
-        //     // renderAudio(jobId, current)
-        // }) 
-        
+        jobId= submitTTSJob(text, current);        
     })
-// async function temp(i){
-//          i = i +1;
-//         console.log("i", i)
-//         if(i < 5)
-//            return await delay(1000, i).then((i)=> temp(i))
-//         else    {
-//         // temp(i)
-//         //    setTimeout(() => temp(i), 1000);
-//         console.log("done") 
-//         return i    
-//         }
-// }
-function delay(t, v) {
-    return new Promise(function(resolve) { 
-        setTimeout(resolve.bind(null, v), t)
-    });
- }
-// async function temp(i) {
-//     let waitTime = 100;
-//     while (true) {
-//         try {
-//             let val = await checkStatus("a195bc0c-8d63-4cbd-8518-2c61a4de6dcf")
-//             // use break or return here to break out of the loop
-//             return val;
-//         } catch (ex) {
-//             // Should also check if the actual error is one that is likely
-//             // temporary.  Otherwise, you may loop forever on a permanent error
-//             console.log("still waiting and trying again")
-//             // implement super simple backoff (there are much more elegant algorithms available)
-//             await delay(waitTime);
-//             waitTime += 300;
-//         }
-//     }
-// }
 
+    function delay(t, v) {
+        return new Promise(function(resolve) { 
+            setTimeout(resolve.bind(null, v), t)
+        });
+    }
 
     function submitTTSJob(text, current){
-
+      toggleLoading(current)
        fetch('https://tts.qcri.org/api/submit_job', {
             method: 'POST',
             headers: {
@@ -161,24 +100,14 @@ function delay(t, v) {
               return response.json()
             })
           .then(function(result){ 
-            
-        //     //   return checkStatus("a195bc0c-8d63-4cbd-8518-2c61a4de6dcf", current)
-        //     new Promise((resolve, reject) => {  
-        //         if(checkStatus(result.job_id)  == result.job_id){
-        //             console.log('result is back')    
-        //             resolve(result.job_id);  
-        //         }
-        //   }).then(jobId=> renderAudio(jobId, current)) 
         promise1 = new Promise(async(resolve, reject) => {
             res = await checkStatus(result.job_id, current)
-            console.log(res)
             if (res == result.job_id){
-                console.log("here")
                  resolve( result.job_id)
             }
         })
         .then((jobId) => {
-            console.log("heeer")
+            toggleLoading(current)
             renderAudio(jobId, current)
         }) 
         })
@@ -186,11 +115,9 @@ function delay(t, v) {
     
     }
     
-    async function checkStatus(jobId, current){
-        
+    async function checkStatus(jobId, current){   
         return fetch('https://tts.qcri.org/api/get_status', {
             method: 'POST',
-            // async: false,
             headers: {
               'Content-Type': 'application/json;charset=utf-8'
             },
@@ -200,9 +127,7 @@ function delay(t, v) {
           }).then(function(response){  
             return response.json();
             }).then(async function(response){ 
-                console.log("let is see", response.status)
              if(response.status !="Done"){
-                // setTimeout(() => checkStatus(jobId, current), 5000);
                 return await delay(5000, jobId).then((jobId)=> checkStatus(jobId, current))
              }
              return jobId;
@@ -211,10 +136,11 @@ function delay(t, v) {
   }
 
   function renderAudio(jobId, current){
-        console.log("here")
-         current.after('<audio controls preload="none" src="https://tts.qcri.org/api/audio/'+jobId+'.wav" >'+
-     
+         current.after('<audio  class="center w-1/2 margin-tb" controls preload="none" src="https://tts.qcri.org/api/audio/'+jobId+'.wav" >'+
         '</audio>')
   }
 
+  function toggleLoading(current){
+    current.siblings('.loading-wrapper').toggleClass('hidden')
+  }
  })
